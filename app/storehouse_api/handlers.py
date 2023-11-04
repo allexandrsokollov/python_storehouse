2cb0ab0466360da20a6e30aca94fc15c6f2387f0
Alex Sokolov <allexandersokollov@gmail.com>
2023-11-04 12:15:27 +0300

    add endpoints to interact with user data

--- app/storehouse_api/handlers.py
+++ app/storehouse_api/handlers.py
@@ -1,10 +1,18 @@
 import uuid
+from typing import List
 
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 
 from app.services.storehouse_services import UserServie, SupplierService, PalletService
-from app.storehouse_api.models import UserModel, CreateUser, CreateSupplierModel, SupplierModel, PalletModel, \
-    CreatePalletModel
+from app.storehouse_api.models import (
+    UserModel,
+    CreateUser,
+    CreateSupplierModel,
+    SupplierModel,
+    PalletModel,
+    CreatePalletModel,
+    UserUpdate,
+)
 
 user_router = APIRouter()
 supplier_router = APIRouter()
@@ -15,6 +23,45 @@ pallet_router = APIRouter()
 async def create_user(user: CreateUser):
     user_service = UserServie()
     return await user_service.create(user)
+
+
+@user_router.get("/{user_id}", response_model=UserModel)
+async def get_user(user_id: uuid.UUID):
+    user_service = UserServie()
+    user = await user_service.retrieve(user_id)
+
+    if not user:
+        raise HTTPException(
+            status_code=400, detail=f"User with this id: {user_id} does not exists"
+        )
+
+    return user
+
+
+@user_router.put("{user_id}", response_model=UserModel)
+async def update_user(user_id: uuid.UUID, user_data: UserUpdate):
+    user_service = UserServie()
+    user = await user_service.update(user_id, user_data)
+
+    if not user:
+        raise HTTPException(
+            status_code=400, detail=f"User with this id: {user_id} does not exists"
+        )
+
+    return user
+
+
+@user_router.delete("user_id")
+async def delete_user(user_id: uuid.UUID):
+    user_service = UserServie()
+    res = await user_service.delete(user_id)
+
+    if not res:
+        raise HTTPException(
+            status_code=400, detail=f"User with this id: {user_id} does not exists"
+        )
+
+    return {"success": True}
 
 
 @supplier_router.post("/", response_model=SupplierModel)
@@ -33,3 +80,10 @@ async def create_supplier(supplier_id: uuid.UUID):
 async def create_pallet(pallet: CreatePalletModel):
     pallet_service = PalletService()
     return await pallet_service.create(pallet)
+
+
+@pallet_router.get("/", response_model=List[PalletModel])
+async def get_pallets():
+    pallet_service = PalletService()
+    pallets = await pallet_service.get_all()
+    return pallets
